bfd02fcb2344bf3d8dbc7e089ad19414c817d76a
Otieno-ph <omondifelix227@gmail.com>
2023-02-23 13:14:39 -0800

    more nestedd

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,41 +1,48 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
-  *print_number - prints an integer
-  *only use _putchar
-  *do not use long
-  *no arrays or pointers
-  *no hard-code special values
-  *@n: integer
-  *Return: success
-  */
-
-
+ * print_number - prints an integer
+ * @n: random integer
+ * Return: void
+ */
 void print_number(int n)
 {
-unsigned int x, y, count;
+	char ld, cr;
+	int r;
+	int ct = 0;
 
-if (n < 0)
-{
-_putchar(45);
-x = n * -1;
-}
-else
-{
-x = n;
-}
-y = x;
-count = 1;
+	if (n < 0)
+	{
+		_putchar ('-');
+		ld = (char)('0' - (n % 10));
+		n = n / -10;
+	}
 
-while (y > 9)
-{
-y /= 10;
-count *= 10;
-}
+	else
+	{
+		ld = (char)((n % 10) + '0');
+		n = n / 10;
+	}
 
-for (; count >= 1; count /= 10)
-{
-_putchar(((x / count) % 10) + 48);
-}
+	r = 0;
+	while (n > 0)
+	{
+		r = r * 10 + (n % 10);
+		n = n / 10;
+		ct++;
+	}
+
+	while (r > 0)
+	{
+		cr = (char)((r % 10) + '0');
+		_putchar(cr);
+		r = r / 10;
+		ct--;
+	}
+	while (ct != 0)
+	{
+		_putchar('0');
+		ct--;
+	}
+	_putchar(ld);
 }
