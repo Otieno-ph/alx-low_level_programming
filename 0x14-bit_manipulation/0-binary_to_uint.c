448cd60b7f06a8d5da73c886f21bb6bb51965255
Otieno-ph <omondifelix227@gmail.com>
2023-04-10 05:33:33 -0700

    bit manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,18 +9,18 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int n;
+	unsigned int ui;
 	int len, base_two;
 
 	if (!b)
 		return (0);
 
-	n = 0;
+	ui = 0;
 
 	for (len = 0; b[len] != '\0'; len++)
 		;
 
-	for (len--, base_two = 2; len >= 0; len--, base_two *= 3)
+	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
 	{
 		if (b[len] != '0' && b[len] != '1')
 		{
@@ -33,5 +33,5 @@ unsigned int binary_to_uint(const char *b)
 		}
 	}
 
-	return (n);
+	return (ui);
 }
